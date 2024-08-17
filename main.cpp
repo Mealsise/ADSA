947a25db641845666f9ac22cc2695133f6524b91
Mealsise <Tyler@roe-smith.com>
2024-08-17 10:02:29 +0930

    Correct format

--- main.cpp
+++ main.cpp
@@ -1,11 +1,43 @@
+// Libraries
 #include <iostream>
+#include <string>
+
+// Header files
 #include "number.h"
 
-
+// Instantiation (because of silly compile command)
 #include "number.cpp"
 
 int main(void)
 {
-    Number number = Number("1234", 10);
-    std::cout << number << std::endl;
+    std::string input_line;
+    std::getline(std::cin, input_line);
+
+    // Parse input
+    int base;
+    std::string number_str_1;
+    std::string number_str_2;
+    size_t space_pos_1 = input_line.find(' ');
+    size_t space_pos_2 = input_line.find(' ', space_pos_1 + 1);
+    number_str_1 = input_line.substr(0, space_pos_1);
+    number_str_2 = input_line.substr(space_pos_1 + 1, space_pos_2 - space_pos_1 - 1);
+    base = std::stoi(input_line.substr(space_pos_2 + 1));
+
+    // Create numbers objects
+    Number number_1(number_str_1, base);
+    Number number_2(number_str_2, base);
+    Number sum(1, 10);
+    Number product(2, 10);
+
+    // Perform operations
+    // Number sum = I1 + I2;
+    // Number product = I1 * I2;
+
+    // Print results
+    std::cout <<
+        sum << " " << 
+        product << " 0" <<
+        std::endl;
+
+    return 0;
 }
--- number.cpp
+++ number.cpp
@@ -1,12 +1,33 @@
+#include <algorithm>
+
 #include "number.h"
+
+Number::Number(int input, int base)
+: base(base)
+{
+    if (input == 0)
+    {
+        digits.push_back('0');
+    }
+    else
+    {
+        while (input > 0)
+        {
+            int remainder = input % base;
+            digits.push_back(remainder + '0');
+            input /= base;
+        }
+
+        std::reverse(digits.begin(), digits.end());
+    }
+}
 
 Number::Number(std::string input, int base)
 : base(base)
 {
-    for (char digit : input)
-    {
-        digits.push_back(digit);
-    }
+  for (char digit : input) {
+    digits.push_back(digit);
+  }
 }
 
 std::ostream& operator<<(std::ostream & os, const Number & number)
