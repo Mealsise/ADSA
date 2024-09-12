f6e0b2167dffa08052b5764b38337bb05dc2a769
Mealsise <Tyler@roe-smith.com>
2024-09-12 22:19:23 +0930

    a

--- AVL_Tree_Node.h
+++ AVL_Tree_Node.h
@@ -12,152 +12,228 @@ private:
     int value;
     int height;
 
-public: 
-    AVL_Tree_Node(int value)
-        : value(value), left(nullptr), right(nullptr), height(1) {}
+public:
+    // Constructor
+    AVL_Tree_Node(int key) : value(key), left(nullptr), right(nullptr), height(1)
+    {}
 
+    // Destructor
     ~AVL_Tree_Node()
     {
         delete left;
         delete right;
     }
 
-    int getHeight() {return height;}
+    // Get the height of the node
+    int getHeight() const
+    {
+        return height;
+    }
 
-    int getBalance() {return (left ? left->getHeight() : 0) - (right ? right->getHeight() : 0);}
+    // Get balance factor of node to check if it has become unbalanced
+    int getBalance() const
+    {
+        return (left ? left->getHeight() : 0) - (right ? right->getHeight() : 0);
+    }
 
-    void updateHeight() {height = 1 + std::max(left ? left->getHeight() : 0, right ? right->getHeight() : 0);}
+    // Update the height of the node
+    void updateHeight()
+    {
+        height = 1 + std::max(left ? left->getHeight() : 0, right ? right->getHeight() : 0);
+    }
 
-    AVL_Tree_Node* insert(int key) {
+    // Insert a new node with the given key
+    AVL_Tree_Node* insert(int key)
+    {
         if (key < value)
         {
-            if (!left) left = new AVL_Tree_Node(key);
-            else left = left->insert(key);
+            if (!left)
+            {
+                left = new AVL_Tree_Node(key);
+            }
+            else
+            {
+                left = left->insert(key);
+            }
         }
         else if (key > value)
         {
-            if (!right) right = new AVL_Tree_Node(key);
-            else right = right->insert(key);
+            if (!right)
+            {
+                right = new AVL_Tree_Node(key);
+            }
+            else
+            {
+                right = right->insert(key);
+            }
         }
         else
+        {
+            // Key already exists, do nothing
             return this;
+        }
 
-        // Update height after insertion
         updateHeight();
+        return balanceTree();
+    }
 
+    // Remove a node with the given key
+    AVL_Tree_Node* remove(int key)
+    {
+        if (key < value)
+        {
+            if (left != nullptr)
+            {
+                left = left->remove(key);
+            }
+        }
+        else if (key > value)
+        {
+            if (right != nullptr)
+            {
+                right = right->remove(key);
+            }
+        }
+        else
+        {
+            // This is the node to be deleted
+            if (left == nullptr || right == nullptr)
+            {
+                AVL_Tree_Node* temporary = left ? left : right;
+                if (temporary == nullptr)
+                {
+                    temporary = this;
+                    delete temporary;
+                    return nullptr;
+                }
+                else
+                {
+                    *this = *temporary;  // Copy the contents of the non-empty child
+                }
+            }
+            else
+            {
+                // Node with two children: Get the inorder successor
+                AVL_Tree_Node* temporary = right->findMin();
+                value = temporary->value;
+                right = right->remove(temporary->value);
+            }
+        }
+
+        if (this == nullptr) return nullptr;
+
+        updateHeight();
+        return balanceTree();
+    }
+
+    // Find the node with the minimum value
+    AVL_Tree_Node* findMin()
+    {
+        AVL_Tree_Node* current = this;
+        while (current && current->left != nullptr)
+        {
+            current = current->left;
+        }
+        return current;
+    }
+
+    // Rotate right to balance the tree
+    AVL_Tree_Node* rotateRight()
+    {
+        AVL_Tree_Node* x = left;
+        AVL_Tree_Node* T2 = x->right;
+
+        // Perform rotation
+        x->right = this;
+        left = T2;
+
+        // Update heights
+        updateHeight();
+        x->updateHeight();
+
+        return x;
+    }
+
+    // Rotate left to balance the tree
+    AVL_Tree_Node* rotateLeft()
+    {
+        AVL_Tree_Node* y = right;
+        AVL_Tree_Node* T2 = y->left;
+
+        // Perform rotation
+        y->left = this;
+        right = T2;
+
+        // Update heights
+        updateHeight();
+        y->updateHeight();
+
+        return y;
+    }
+
+    // Balance the node according to AVL rules
+    AVL_Tree_Node* balanceTree()
+    {
         int balance = getBalance();
 
-        // Left heavy case
-        if (balance > 1)
+        // Left Left Case
+        if (balance > 1 && getBalance(left) >= 0)
         {
-            if (key < left->value)
-                return rotateRight(); // Left-Left case
-            else if (key > left->value)
-            {
-                left = left->rotateLeft();
-                return rotateRight(); // Left-Right case
-            }
+            return rotateRight();
         }
 
-        // Right heavy case
-        if (balance < -1)
+        // Left Right Case
+        if (balance > 1 && getBalance(left) < 0)
         {
-            if (key > right->value)
-                return rotateLeft(); // Right-Right case
-            if (key < right->value)
-            {
-                right = right->rotateRight();
-                return rotateLeft(); // Right-Left case
-            }
+            left = left->rotateLeft();
+            return rotateRight();
         }
 
-        return this;
+        // Right Right Case
+        if (balance < -1 && getBalance(right) <= 0)
+        {
+            return rotateLeft();
+        }
+
+        // Right Left Case
+        if (balance < -1 && getBalance(right) > 0)
+        {
+            right = right->rotateRight();
+            return rotateLeft();
+        }
+
+        return this;  // No balance needed
     }
 
-    AVL_Tree_Node* findMin() {return left ? left->findMin() : this;}
-
-    AVL_Tree_Node* remove(int key) {
-        if (key < value) {
-            if (left) left = left->remove(key);
-        } else if (key > value) {
-            if (right) right = right->remove(key);
-        } else
-        {
-            // Node to delete found
-            if (!left || !right) 
-            {
-                // One child or no child case
-                AVL_Tree_Node* temp = left ? left : right;
-                delete this;
-                return temp;
-            }
-            else
-            {
-                // Node with two children
-                AVL_Tree_Node* temp = right->findMin();
-                value = temp->value;
-                right = right->remove(temp->value);
-            }
-        }
-
-        // Update height after deletion
-        updateHeight();
-
-        int balance = getBalance();
-
-        // Left heavy case
-        if (balance > 1)
-        {
-            if (left->getBalance() >= 0)
-                return rotateRight(); // Left-Left case
-            else
-            {
-                left = left->rotateLeft();
-                return rotateRight(); // Left-Right case
-            }
-        }
-
-        // Right heavy case
-        if (balance < -1)
-        {
-            if (right->getBalance() <= 0)
-                return rotateLeft(); // Right-Right case
-            else
-            {
-                right = right->rotateRight();
-                return rotateLeft(); // Right-Left case
-            }
-        }
-
-        return this;
+    // Utility to calculate balance factor of a child node
+    int getBalance(AVL_Tree_Node* N)
+    {
+        if (!N) return 0;
+        return N->getBalance();
     }
 
-    void preorder(){std::cout << value << " "; if (left) left->preorder(); if (right) right->preorder();}
-
-    void inorder(){if (left) left->inorder(); std::cout << value << " "; if (right) right->inorder();}
-
-    void postorder() {if (left) left->postorder(); if (right) right->postorder(); std::cout << value << " ";}
-
-    AVL_Tree_Node* getLeft() {return left;}
-
-    AVL_Tree_Node* getRight() {return right;}
-
-    AVL_Tree_Node* rotateRight() {
-        AVL_Tree_Node* newRoot = left;
-        left = newRoot->right;
-        newRoot->right = this;
-        updateHeight();
-        newRoot->updateHeight();
-        return newRoot;
+    // Preorder traversal of the tree
+    void preorder()
+    {
+        std::cout << value << " ";
+        if (left) left->preorder();
+        if (right) right->preorder();
     }
 
-    AVL_Tree_Node* rotateLeft() {
-        AVL_Tree_Node* newRoot = right;
-        right = newRoot->left;
-        newRoot->left = this;
-        updateHeight();
-        newRoot->updateHeight();
-        return newRoot;
+    // Inorder traversal of the tree
+    void inorder()
+    {
+        if (left) left->inorder();
+        std::cout << value << " ";
+        if (right) right->inorder();
+    }
+
+    // Postorder traversal of the tree
+    void postorder()
+    {
+        if (left) left->postorder();
+        if (right) right->postorder();
+        std::cout << value << " ";
     }
 };
 
--- main.cpp
+++ main.cpp
@@ -3,56 +3,71 @@
 #include <string>
 #include "AVL_Tree_Node.h"
 
+// Function to trim leading and trailing spaces from a string
+std::string trim(const std::string& str) {
+    size_t first = str.find_first_not_of(' ');
+    if (first == std::string::npos) return ""; // Empty or all spaces
+    size_t last = str.find_last_not_of(' ');
+    return str.substr(first, last - first + 1);
+}
+
 int main() {
     std::string input;
-    std::getline(std::cin, input); // Get the whole input line
-    std::istringstream iss(input);
-    std::string command;
-    
     AVL_Tree_Node* root = nullptr;
+    
+    // Keep reading until end of input
+    while (std::getline(std::cin, input)) {
+        std::istringstream iss(input);
+        std::string command;
 
-    while (iss >> command) {
-        if (command[0] == 'A') {
-            // Insert command
-            int key = std::stoi(command.substr(1));
-            if (root == nullptr) {
-                root = new AVL_Tree_Node(key);
-            } else {
-                root = root->insert(key);
-            }
-        } else if (command[0] == 'D') {
-            // Delete command
-            int key = std::stoi(command.substr(1));
-            if (root != nullptr) {
-                root = root->remove(key);
-            }
-        } else if (command == "PRE") {
-            // Preorder traversal
-            if (root != nullptr) {
-                root->preorder();
-                std::cout << std::endl;
-            } else {
-                std::cout << "EMPTY" << std::endl;
-            }
-        } else if (command == "IN") {
-            // Inorder traversal
-            if (root != nullptr) {
-                root->inorder();
-                std::cout << std::endl;
-            } else {
-                std::cout << "EMPTY" << std::endl;
-            }
-        } else if (command == "POST") {
-            // Postorder traversal
-            if (root != nullptr) {
-                root->postorder();
-                std::cout << std::endl;
-            } else {
-                std::cout << "EMPTY" << std::endl;
+        while (iss >> command) {
+            command = trim(command);
+            // std::cout << command << std::endl;
+            if (command[0] == 'A') {
+                // Insert command
+                int key = std::stoi(command.substr(1));
+                if (root == nullptr) {
+                    root = new AVL_Tree_Node(key);
+                } else {
+                    root = root->insert(key);
+                }
+            } else if (command[0] == 'D') {
+                // Delete command
+                int key = std::stoi(command.substr(1));
+                if (root != nullptr) {
+                    root = root->remove(key);
+                }
+            } else if (command == "PRE") {
+                // Preorder traversal
+                if (root != nullptr) {
+                    root->preorder();
+                    std::cout << std::endl;
+                } else {
+                    std::cout << "EMPTY" << std::endl;
+                }
+            } else if (command == "IN") {
+                // Inorder traversal
+                if (root != nullptr) {
+                    root->inorder();
+                    std::cout << std::endl;
+                } else {
+                    std::cout << "EMPTY" << std::endl;
+                }
+            } else if (command == "POST") {
+                // Postorder traversal
+                if (root != nullptr) {
+                    root->postorder();
+                    std::cout << std::endl;
+                } else {
+                    std::cout << "EMPTY" << std::endl;
+                }
             }
         }
     }
-
     delete root; // Clean up memory
     return 0;
 }
+
+
+
+
